18887a32ec5eef9716fe5a756fd3a540d7cdae9c
itstanishq <tanishq8562@gmail.com>
2023-11-23 00:18:11 +0530

    create a test api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,12 +14,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type : String,
         required : true,
-        
+            
     }
 }, {timestamps : true});
 
 const User = mongoose.model("User", userSchema);
 
 export default User;
-
 
